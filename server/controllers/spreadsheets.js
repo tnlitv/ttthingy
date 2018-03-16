ca09c1635e6c8f4c361ce07e959419ee11eea1f1
tn <tatyana@preen.me>
2018-03-16 14:38:05 +0200

    add spreadsheets api

--- server/controllers/spreadsheets.js
+++ server/controllers/spreadsheets.js
@@ -7,39 +7,50 @@ const scopes = [
 
 const getSpreadsheet = async function (req, res) {
     try {
-        let key = require('../../ttthingy-service-account-key.json');
-
-        let jwtClient = new google.auth.JWT({
-                email: key.client_email,
-                key: key.private_key,
-                scopes: 'https://www.googleapis.com/auth/spreadsheets'
-            }
-        );
-
-        await new Promise((resolve, reject) => {
-            jwtClient.authorize((err, tokens) => {
-                if (err) reject(err);
-                resolve(tokens);
-            });
-        });
-
-        let spreadsheet = await new Promise((resolve, reject) => {
-            sheets.spreadsheets.values.get({
-                auth: jwtClient,
-                spreadsheetId: '1vxokkFR0GqdHe1865hwmlut8NdU7g8meMpsHuo7vDu8',
-                range: 'A1:B1',
-            }, function (err, resp) {
-                if (err) reject(err);
-                resolve(resp.data);
-            })
-        });
-
-        let rows = spreadsheet.values;
-        return res.json({rows});
+        let rows = [
+            ["2", "Tanya", "8", "", "", "", "", "MAT-88"]
+        ];
+        let updatedCells = await addRowToSpreadsheet(rows);
+        return res.json({updatedCells});
     } catch (e) {
         console.log(e);
         return res.status(500).send(e.errors);
     }
+};
+
+let addRowToSpreadsheet = async function (values) {
+    let key = require('../../ttthingy-service-account-key.json');
+
+    let jwtClient = new google.auth.JWT({
+            email: key.client_email,
+            key: key.private_key,
+            scopes: 'https://www.googleapis.com/auth/spreadsheets'
+        }
+    );
+
+    await new Promise((resolve, reject) => {
+        jwtClient.authorize((err, tokens) => {
+            if (err) reject(err);
+            resolve(tokens);
+        });
+    });
+
+    let spreadsheet = await new Promise((resolve, reject) => {
+        sheets.spreadsheets.values.append({
+            auth: jwtClient,
+            spreadsheetId: process.env.SPREADSHEET_ID,
+            range: 'A1:A4000',
+            valueInputOption: "USER_ENTERED",
+            resource: {
+                values: values
+            }
+        }, (err, resp) => {
+            if (err) reject(err);
+            resolve(resp);
+        })
+    });
+
+    return spreadsheet.updatedCells;
 };
 
 module.exports = {
